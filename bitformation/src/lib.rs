b1252bcad61fa99d8a2dc6373bf25acc578bcb26
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-11-12 09:43:17 +0200

    feat: added rest of bitf impls

--- bitformation/src/lib.rs
+++ bitformation/src/lib.rs
@@ -7,7 +7,15 @@ pub struct Bitf(i64);
 
 impl Into<Bitf> for Iter<'_, i8> {
     fn into(self) -> Bitf {
-        todo!()
+        assert_eq!(self.len(), 8);
+
+        let mut bitf = 0i64;
+        self.for_each(|n| {
+            bitf <<= 8;
+            bitf |= *n as i64;
+        });
+
+        Bitf(bitf)
     }
 }
 
@@ -15,18 +23,11 @@ impl Into<Bitf> for Iter<'_, i16> {
     fn into(self) -> Bitf {
         assert_eq!(self.len(), 4);
 
-        // let mut bitf: i64 = 0;
-        // bitf <<= 8;
-        // bitf |= self.as_slice()[0] as i64;
-        // println!("{bitf:064b}");
         let mut bitf = 0i64;
-        // bitf <<= 8;
-        // bitf |= self.as_slice()[0] as i64;
         self.for_each(|n| {
             bitf <<= 16;
             bitf |= *n as i64;
         });
-        println!("{bitf:064b}");
 
         Bitf(bitf)
     }
@@ -34,11 +35,14 @@ impl Into<Bitf> for Iter<'_, i16> {
 
 impl Into<Bitf> for Iter<'_, i32> {
     fn into(self) -> Bitf {
-        todo!()
+        assert_eq!(self.len(), 2);
+
+        let mut bitf = 0i64;
+        self.for_each(|n| {
+            bitf <<= 32;
+            bitf |= *n as i64;
+        });
+
+        Bitf(bitf)
     }
 }
-
-// 0000000001111101 
-// 0000000000000010
-// 0000000000000011
-// 0000000000000100
